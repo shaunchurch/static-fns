268b48efb11cbee7bd3e1ef7f463a226e52ad3ee
Shaun Church <shaun@shaunchurch.com>
2020-06-10 08:15:18 +0100

    Export PostData type

--- packages/blog/index.ts
+++ packages/blog/index.ts
@@ -1,3 +1,3 @@
-export { getPosts, getTags, getAuthors } from "./src/blog-engine";
+export { getPosts, getTags, getAuthors, PostData } from "./src/blog-engine";
 export { generateRSS } from "./src/build-rss";
 export { slugify } from "./src/slugify";
