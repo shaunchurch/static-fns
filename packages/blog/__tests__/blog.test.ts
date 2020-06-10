b885bc6615c8b4487a53fd1d3d1681cd7718ba2a
Shaun Church <shaun@shaunchurch.com>
2020-06-10 14:42:16 +0100

    Add empty passing tests

---
+++ packages/blog/__tests__/blog.test.ts
@@ -0,0 +1,7 @@
+describe("blog", () => {
+  it("needs tests", () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  it("should read a set of posts from the filesystem and return an array", () => {});
+});
