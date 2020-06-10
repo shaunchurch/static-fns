17a2daba3d1b54182d0cd7f4f87db29d1d1cae6f
Shaun Church <shaun@shaunchurch.com>
2020-06-10 09:24:29 +0100

    Export fetcher

--- packages/blog/index.ts
+++ packages/blog/index.ts
@@ -2,3 +2,4 @@ export { getPosts, getTags, getAuthors, PostData } from "./src/blog-engine";
 export { generateRSS } from "./src/build-rss";
 export { slugify } from "./src/slugify";
 export { dateFormat } from "./src/date-functions";
+export { default as fetcher } from "./src/fetcher";
