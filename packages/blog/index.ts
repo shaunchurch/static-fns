0aed478b99a54e21a193c2e01b9c2aba3083959d
Shaun Church <shaun@shaunchurch.com>
2020-06-10 08:06:07 +0100

    Elevate blog package index

---
+++ packages/blog/index.ts
@@ -0,0 +1,3 @@
+export { getPosts, getTags, getAuthors } from "./src/blog-engine";
+export { generateRSS } from "./src/build-rss";
+export { slugify } from "./src/slugify";
--- packages/blog/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { getPosts, getTags, getAuthors } from "./blog-engine";
-export { generateRSS } from "./build-rss";
-export { slugify } from "./slugify";
