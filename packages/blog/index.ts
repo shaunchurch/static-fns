bedb8c5d07535c175cd41bb39e728bfb0ce6ddce
Shaun Church <shaun@shaunchurch.com>
2020-06-10 08:56:55 +0100

    Expose date format function

--- packages/blog/index.ts
+++ packages/blog/index.ts
@@ -1,3 +1,4 @@
 export { getPosts, getTags, getAuthors, PostData } from "./src/blog-engine";
 export { generateRSS } from "./src/build-rss";
 export { slugify } from "./src/slugify";
+export { dateFormat } from "./src/date-functions";
