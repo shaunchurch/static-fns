d11675b7434a7a86290828210d30f5d908006b72
Shaun Church <shaun@shaunchurch.com>
2020-06-12 12:37:34 +0100

    Export deSlugify

--- packages/blog/index.ts
+++ packages/blog/index.ts
@@ -1,5 +1,5 @@
 export { getPosts, getTags, getAuthors, PostData } from "./src/blog-fns";
 export { generateRSS } from "./src/build-rss";
-export { slugify } from "./src/slugify";
+export { slugify, deSlugify } from "./src/slugify";
 export { dateFormat } from "./src/date-fns";
 export { default as fetcher } from "./src/fetcher";
