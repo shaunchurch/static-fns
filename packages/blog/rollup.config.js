1231b4e4f4cc53e1df7b705f7a4f384a1e522870
Shaun Church <shaun@shaunchurch.com>
2020-07-22 13:33:06 +0100

    Update rollup plugins

--- packages/blog/rollup.config.js
+++ packages/blog/rollup.config.js
@@ -1,6 +1,7 @@
-import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
-import nodeResolve from "rollup-plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+// import commonjs from "rollup-plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
 
 export default {
   input: "index.ts",
