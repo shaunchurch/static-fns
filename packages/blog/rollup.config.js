ba1f9b73252a8033c3756589c80ce45c29eaf9f3
Shaun Church <shaun@shaunchurch.com>
2020-07-22 14:10:42 +0100

    Random changes to try getting bundling to not be total dogshit

--- packages/blog/rollup.config.js
+++ packages/blog/rollup.config.js
@@ -1,29 +1,72 @@
 import typescript from "@rollup/plugin-typescript";
-// import commonjs from "rollup-plugin-commonjs";
 import commonjs from "@rollup/plugin-commonjs";
-import nodeResolve from "@rollup/plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+import pkg from "./package.json";
 
-export default {
-  input: "index.ts",
-  output: [
-    {
-      file: "lib/index.js",
-      format: "cjs",
+export default [
+  // browser-friendly UMD build
+  {
+    input: "index.ts",
+    output: {
+      name: "static-fns",
+      // file: pkg.browser,
+      dir: "./lib",
+      format: "umd",
     },
-    {
-      file: "lib/index.es.js",
-      format: "esm",
-    },
-  ],
-  plugins: [
-    nodeResolve(),
-    commonjs({
-      include: [
-        "node_modules/@static-fns/**",
-        "node_modules/rss/**",
-        "node_modules/isomorphic-unfetch/**",
-      ],
-    }),
-    typescript(),
-  ],
-};
+    plugins: [
+      resolve(), // so Rollup can find `ms`
+      commonjs(), // so Rollup can convert `ms` to an ES module
+      typescript({ outDir: "./lib" }),
+      json(),
+    ],
+  },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: "index.ts",
+    external: ["ms"],
+    output: [
+      {
+        // file: pkg.main,
+        dir: "./lib",
+        format: "cjs",
+      },
+      {
+        // file: pkg.module,
+        dir: "./lib",
+        format: "es",
+      },
+    ],
+  },
+];
+
+// export default {
+//   input: "index.ts",
+//   output: [
+//     {
+//       file: "index.js",
+//       format: "cjs",
+//     },
+//     {
+//       file: "index.es.js",
+//       format: "esm",
+//     },
+//   ],
+//   plugins: [
+//     nodeResolve(),
+//     commonjs({
+//       include: [
+//         "node_modules/@static-fns/**",
+//         "node_modules/rss/**",
+//         "node_modules/isomorphic-unfetch/**",
+//       ],
+//     }),
+//     typescript(),
+//   ],
+// };
