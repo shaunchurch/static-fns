9b175365253d28ea8f3cce539b5f91adfc7dc932
Shaun Church <shaun@shaunchurch.com>
2020-07-23 22:39:53 +0100

    paths and dirs and tigers

--- packages/blog/rollup.config.js
+++ packages/blog/rollup.config.js
@@ -2,19 +2,30 @@ import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
-export default {
-  input: "index.ts",
-
-  output: [
-    // { file: "dist/bundle.cjs.js", dir: "./dist", format: "cjs" },
-    {
-      //file: "dist/bundle.es.js",
-      dir: "./dist",
-      format: "es",
-    },
-    // { file: "dist/bundle.umd.js", format: "umd", name: "static-fns" },
-  ],
-
-  // plugins: [nodeResolve(), commonjs(), typescript()],
-  plugins: [typescript({ declarationDir: "./dist" })],
-};
+export default [
+  {
+    input: "index.ts",
+    output: [
+      {
+        dir: "./dist/es",
+        format: "es",
+      },
+    ],
+    // plugins: [nodeResolve(), commonjs(), typescript()],
+    plugins: [typescript({ declarationDir: "./dist/es", outDir: "./dist/es" })],
+  },
+  {
+    input: "index.ts",
+    output: [
+      {
+        //file: "dist/bundle.es.js",
+        dir: "./dist/cjs",
+        format: "cjs",
+      },
+    ],
+    // plugins: [nodeResolve(), commonjs(), typescript()],
+    plugins: [
+      typescript({ declarationDir: "./dist/cjs", outDir: "./dist/cjs" }),
+    ],
+  },
+];
