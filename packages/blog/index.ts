e1cd3f7697ccb513193f6215bdaa32831b8c1edd
Shaun Church <shaun@shaunchurch.com>
2020-07-25 12:50:16 +0100

    Restore slugify

--- packages/blog/index.ts
+++ packages/blog/index.ts
@@ -9,6 +9,6 @@ export {
   PostData,
 } from "./src/blog-fns";
 export { generateRSS } from "./src/build-rss";
-// export { slugify, deSlugify } from "./src/slugify";
+export { slugify, deSlugify } from "./src/slugify";
 export { dateFormat } from "./src/date-fns";
 export { default as fetcher } from "./src/fetcher";
