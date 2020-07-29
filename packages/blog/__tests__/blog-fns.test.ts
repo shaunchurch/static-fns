a85cf9acfe73cee27f221298b53fd1f62bfb3ac6
Alex Scotton <alex@alexscotton.com>
2020-07-29 10:33:18 +0100

    Add test to ensure excerpt extraction removes markdown syntax

--- packages/blog/__tests__/blog-fns.test.ts
+++ packages/blog/__tests__/blog-fns.test.ts
@@ -99,6 +99,13 @@ describe("blog-fns", () => {
     expect(posts[0].excerpt).toBe("Hello welcome to the first test post");
   });
 
+  it("should provide an automatic excerpt for a post and remove markdown syntax", () => {
+    const posts = getPosts();
+
+    expect(getPostByKey(posts, "title", "A Complicated Post").excerpt)
+      .toBe("A really early link Strong Bold Text followed by Italics A quotation")
+  });
+
   it("should respect an existing excerpt for a post", () => {
     const posts = getPosts();
 
